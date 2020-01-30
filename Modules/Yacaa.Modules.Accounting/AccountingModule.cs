4c349f5cddb19dc9bb2bea15dd55c432e4eb08c3
etherform <petr.komogortsev@gmail.com>
2020-01-30 10:34:13 +0300

    application structure

---
+++ Modules/Yacaa.Modules.Accounting/AccountingModule.cs
@@ -0,0 +1,20 @@
+﻿using Yacaa.Modules.Accounting.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace Yacaa.Modules.Accounting
+{
+    public class AccountingModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+ 
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
---
+++ Modules/Yacaa.Modules.Analytics/AnalyticsModule.cs
@@ -0,0 +1,20 @@
+﻿using Yacaa.Modules.Analytics.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace Yacaa.Modules.Analytics
+{
+    public class AnalyticsModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+ 
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
