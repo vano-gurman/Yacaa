156ff6b23e5c9c2e3d1a653eee9ea83f1947d5b6
etherform <petr.komogortsev@gmail.com>
2020-02-04 10:36:15 +0300

    Trying out DataGrid

--- Modules/Yacaa.Modules.Accounting/ViewModels/PartnerInteraction/ContractsViewModel.cs
+++ Modules/Yacaa.Modules.Accounting/ViewModels/PartnerInteraction/ContractsViewModel.cs
@@ -1,12 +1,27 @@
-﻿using Prism.Mvvm;
+﻿using System.Collections.ObjectModel;
+using Prism.Mvvm;
+using Yacaa.Shared.Models;
 
 namespace Yacaa.Modules.Accounting.ViewModels.PartnerInteraction
 {
     public class ContractsViewModel : BindableBase
     {
+        public ObservableCollection<Contract> Contracts { get; set; }
+        public bool ReadOnly { get; set; } = true;
         public ContractsViewModel()
         {
+            Contracts = GenerateData();
+        }
 
+        private ObservableCollection<Contract> GenerateData()
+        {
+            return new ObservableCollection<Contract>()
+            {
+                new Contract("#111221", "12/01/2019", "Incoming", "RUB", "28/01/2020", "YES"),
+                new Contract("#111222", "13/01/2019", "Outgoing", "USD", "29/01/2020", "NO"),
+                new Contract("#111223", "14/01/2019", "Incoming", "RUB", "30/01/2020", "NO"),
+                new Contract("#111224", "15/01/2019", "Incoming", "USD", "31/01/2020", "YES")
+            };
         }
     }
 }
---
+++ Yacaa.Shared/Models/Contract.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Yacaa.Shared.Models
+{
+    public class Contract
+    {
+        public string Number { get; set; }
+        public string Date { get; set; }
+        public string ContractType { get; set; }
+        public string Currency { get; set; }
+        public string ValidUntil { get; set; }
+        public string Prolongation { get; set; }
+        public string ProlongationTerms { get; set; } = null;
+
+        public Contract(string number, string date, string contractType, string currency, string validUntil, string prolongation, string prolongationTerms)
+        {
+            Number = number;
+            Date = date;
+            ContractType = contractType;
+            Currency = currency;
+            ValidUntil = validUntil;
+            Prolongation = prolongation;
+            ProlongationTerms = prolongationTerms;
+        }
+
+        public Contract(string number, string date, string contractType, string currency, string validUntil, string prolongation)
+        {
+            Number = number;
+            Date = date;
+            ContractType = contractType;
+            Currency = currency;
+            ValidUntil = validUntil;
+            Prolongation = prolongation;
+        }
+        
+    }
+}
