d1e6840e57479f56e23e913b6f2950e4dbe7190e
etherform <petr.komogortsev@gmail.com>
2020-02-09 00:02:01 +0300

    initial db models

---
+++ Services/Yacaa.Services.DataAccess/Contexts/ContractsContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Yacaa.Shared.Models;
+using Yacaa.Shared.Models.Dictionaries;
+
+namespace Yacaa.Services.DataAccess.Contexts
+{
+    public class ContractsContext : DbContext
+    {
+        public ContractsContext(DbContextOptions options) : base (options) { }
+
+        public DbSet<Contract> Contracts { get; set; }
+        public DbSet<ContractType> ContractTypes { get; set; }
+        public DbSet<Currency> Currencies { get; set; }
+    }
+}
---
+++ Yacaa.Shared/Models/Contract.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Yacaa.Shared.Models.Dictionaries;
+
+namespace Yacaa.Shared.Models
+{
+    public class Contract
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Number { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public ContractType ContractType { get; set; }
+        [Required]
+        public Currency Currency { get; set; }
+        [Required]
+        public DateTime ValidUntil { get; set; }
+        [Required]
+        public bool Prolongation { get; set; }
+        [MaxLength(200)]
+        public string ProlongationTerms { get; set; } = null;
+
+    }
+}
